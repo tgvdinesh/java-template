fc84d5dd387a2a9d79fb9c0eb698998edab4555c
Dinesh V <tgvdinesh@gmail.com>
2018-04-19 22:02:44 +0530

    Complete multiple test case assert functionality

--- src/test/java/com/example/java/AppTest.java
+++ src/test/java/com/example/java/AppTest.java
@@ -7,10 +7,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -24,12 +22,10 @@ public class AppTest {
     private static final String INPUT = "/input.txt";
     private static final String OUTPUT = "/output.txt";
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     @Before
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
     }
 
     @After
@@ -39,23 +35,10 @@ public class AppTest {
     }
 
     @Test
-    public void out() {
-        System.out.print("hello");
-        assertEquals("hello", outContent.toString());
-    }
-
-    @Test
-    public void err() {
-        System.err.print("hello again");
-        assertEquals("hello again", errContent.toString());
-    }
-
-    @Test
     public void runSingleTestCase() throws Exception {
         File inputFile = new File(AppTest.class.getResource(INPUT).toURI());
         final FileInputStream fileInputStream = new FileInputStream(inputFile);
         System.setIn(fileInputStream);
-        // Get expected output
         String expectedOutput = new String(Files.readAllBytes(Paths.get(App.class.getResource(OUTPUT).toURI())));
         App.main(null);
         assertEquals(expectedOutput, outContent.toString().trim());
@@ -66,6 +49,16 @@ public class AppTest {
         String testCasesAsString = FileUtility.readFromResourcesDirectory(FILE, getClass().getClassLoader());
         ObjectMapper objectMapper = new ObjectMapper();
         TestCase[] testCases = objectMapper.readValue(testCasesAsString, TestCase[].class);
-        assertEquals(2, testCases.length);
+        if (testCases.length > 1) {
+            for (TestCase testCase : testCases) {
+                InputStream stream = new ByteArrayInputStream(testCase.getInput().getBytes(StandardCharsets.UTF_8));
+                System.setIn(stream);
+                App.main(null);
+                assertEquals(testCase.getOutput().trim(), outContent.toString().trim());
+                outContent.reset();
+            }
+        } else {
+            System.err.println("You do not have multiple test case. Please run singe test case!");
+        }
     }
 }
